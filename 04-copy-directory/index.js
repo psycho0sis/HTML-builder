01998520ece8d0dc978f29920bf96f773357b73b
Ilona <ilonakot92@gmail.com>
2021-11-06 23:59:03 +0300

    refactor: change 04 task solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,22 +1,40 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 
-console.log(__dirname + "/files-copy")
+const fsProm = require("fs").promises;
+
 fs.mkdir(__dirname + "/files-copy", { recursive: true }, (err) => {
   if (err) throw err;
+});
+
+fs.readdir(__dirname + "/files-copy/", function (err, files) {
+  async function deteleFiles() {
+    try {
+      for await (let file of files) {
+        fsProm.unlink(__dirname + "/files-copy/" + path.basename(file));
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  deteleFiles();
 });
 
 const filePath = __dirname + "/files";
 
 fs.readdir(filePath, function (err, files) {
-    if (err) throw err;
-    files.forEach(file => {
-        fs.copyFile(path.join(__dirname + "/files/" + file), path.join(__dirname + "/files-copy/" + path.basename(file)), err => {
-            if(!err){
-                console.log(file + " has been copied!");
-            } else {
-                throw err;
-            }
-        })
-    });
-})
+  if (err) throw err;
+  files.forEach((file) => {
+    fs.copyFile(
+      path.join(__dirname + "/files/" + file),
+      path.join(__dirname + "/files-copy/" + path.basename(file)),
+      (err) => {
+        if (!err) {
+          console.log(file + " has been copied!");
+        } else {
+          throw err;
+        }
+      }
+    );
+  });
+});
